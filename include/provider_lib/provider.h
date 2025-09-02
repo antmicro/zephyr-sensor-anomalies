8c37e4593abf9a9bf1c8d55b2c082f651e37f8cc
Mikolaj Klikowicz <mklikowicz@antmicro.com>
2025-09-02 15:51:15 +0200

    [#81288] provider_lib: Introduce docstrings
    Signed-off-by: Mikolaj Klikowicz <mklikowicz@antmicro.com>

--- include/provider_lib/provider.h
+++ include/provider_lib/provider.h
@@ -17,16 +17,20 @@ enum provider_status
 struct provider
 {
     enum sensor_channel *channels;
+    /* Count of `channels` */
     uint32_t channels_N;
-
+    /* Read function returning `channels_N` floats */
     int (*read_fn)(struct provider *, float *);
+    /* Opaque provider data */
     void *info;
 };
 
 struct provider_reader
 {
     struct provider *ps[CONFIG_MAX_PROVIDER_COUNT];
+    /* Number of providers */
     uint32_t ps_N;
+    /* Total number channels */
     uint32_t dst_N;
 };
 
@@ -36,10 +40,27 @@ struct provider_hdr_entry
     enum sensor_channel chan;
 };
 
+/**
+ * Registers a single provider in the provider reader and increments `dst_N`.
+ *
+ * Usually called from helpers like `provider_reader_register_all_sensor`.
+ */
 int provider_reader_register(struct provider_reader *pr, struct provider *p);
 
+/**
+ * Reads from all providers in the provider reader using `read_fn` and collates results into a float buffer.
+ *
+ * `dst` size must be greater or equal than `dst_N`.
+ */
 int provider_reader_read_all(struct provider_reader *pr, float *dst);
 
+/**
+ * Generates a header containing information on provider id and channel of every element in the float buffer
+ * returned by `provider_reader_read_all`.
+ *
+ * `hdr` size must be greater or equal than `dst_N`. Header doesn't change between reads
+ * and should be generated only after provider registration.
+ */
 int provider_reader_hdr(struct provider_reader *pr, struct provider_hdr_entry *hdr);
 
 #endif // PROVIDER_LIB_PROVIDER_
