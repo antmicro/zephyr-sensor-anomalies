4b7cd728bec2c6669e791aec22b87c76a76a2568
Mikolaj Klikowicz <mklikowicz@antmicro.com>
2025-11-27 14:19:12 +0100

    [#86972] Support EllipticEnvelope and DecisionTree
    Signed-off-by: Mikolaj Klikowicz <mklikowicz@antmicro.com>

--- scripts/gen_emlearn.py
+++ scripts/gen_emlearn.py
@@ -16,32 +16,109 @@ import sys
 import joblib
 import jinja2
 
+from sklearn.covariance import EllipticEnvelope
 from sklearn.mixture import GaussianMixture
+from sklearn.tree import DecisionTreeClassifier
 
-estimator: GaussianMixture = joblib.load(sys.argv[1])
-
-features = estimator.n_features_in_
-components = len(estimator.means_)
-
-cmodel = emlearn.convert(estimator, method="loadable")
-src = cmodel.save("g_emlearn_model")
+THRESHOLD = float(sys.argv[3]) if len(sys.argv) > 3 else 50.0
 
 
-template_str = """
-{{ src }}
+def prepare_gaussian_mixture(clf: GaussianMixture):
+    features = clf.n_features_in_
+    components = len(clf.means_)
 
-int32_t detector_emlearn_detect_impl(const float * buf, float * score) {
-    static float probs[{{ components }}];
-    EmlError status;
+    cmodel = emlearn.convert(clf, method="loadable")
+    src = cmodel.save("g_emlearn_model")
 
-    status = g_emlearn_model_score(buf, {{ features }}, probs, score);
+    template_str = """
+    {{ src }}
 
-    return 0;
-}
-"""
+    int32_t detector_emlearn_detect_impl(const float * buf, float * score) {
+        static float probs[{{ components }}];
+        EmlError status;
 
-env = jinja2.Environment()
-template = env.from_string(template_str)
-rendered = template.render(src=src, components=components, features=features)
+        status = g_emlearn_model_score(buf, {{ features }}, probs, score);
 
-Path(sys.argv[2]).write_text(rendered)
+        *score = *score > {{ threshold }};
+
+        return 0;
+    }
+    """
+
+    env = jinja2.Environment()
+    template = env.from_string(template_str)
+    rendered = template.render(
+        src=src, components=components, features=features, threshold=THRESHOLD
+    )
+
+    return rendered
+
+
+def prepare_elliptic_envelope(clf: EllipticEnvelope):
+    features = clf.n_features_in_
+
+    cmodel = emlearn.convert(clf, method="inline")
+    src = cmodel.save("g_emlearn_model")
+
+    template_str = """
+    {{ src }}
+
+    int32_t detector_emlearn_detect_impl(const float * buf, float * score) {
+        float dist;
+        *score = !eml_elliptic_envelope_predict(&g_emlearn_model_classifier, buf, {{ features }}, &dist);
+        return 0;
+    }
+    """
+
+    env = jinja2.Environment()
+    template = env.from_string(template_str)
+    rendered = template.render(src=src, features=features, threshold=THRESHOLD)
+
+    return rendered
+
+
+def prepare_decision_tree(clf: DecisionTreeClassifier):
+    features = clf.n_features_in_
+
+    cmodel = emlearn.convert(clf, method="inline")
+    src = cmodel.save("g_emlearn_model")
+
+    template_str = """
+    {{ src }}
+
+    int32_t detector_emlearn_detect_impl(const float * buf, float * score) {
+        int16_t qbuf[{{ features }}];
+        for (int i = 0; i < {{ features }}; i++) {
+            qbuf[i] = buf[i] * 1000;
+        }
+        float pred = g_emlearn_model_predict(qbuf, {{ features }});
+        *score = 1 - pred;
+        return 0;
+    }
+    """
+
+    env = jinja2.Environment()
+    template = env.from_string(template_str)
+    rendered = template.render(src=src, features=features)
+
+    return rendered
+
+
+def main():
+    template_map = {
+        GaussianMixture: prepare_gaussian_mixture,
+        EllipticEnvelope: prepare_elliptic_envelope,
+        DecisionTreeClassifier: prepare_decision_tree,
+    }
+
+    estimator = joblib.load(sys.argv[1])
+
+    rendered = template_map[type(estimator)](estimator)
+
+    print(rendered)
+
+    Path(sys.argv[2]).write_text(rendered)
+
+
+if __name__ == "__main__":
+    main()
